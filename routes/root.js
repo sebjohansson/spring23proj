14321cac1c725e628e8ebbf8f572fc05276df365
KayJayTee <kj.tjust@protonmail.com>
2023-01-12 11:06:38 +0100

    Initial buildup for node.js server
    used terminal commands:
    
    $ npm init -y
    $ npm i express
    $ npm i nodemon -D

---
+++ routes/root.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+
+router.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
+})
+
+module.exports = router
+
+// ^ Beginning of string
+// $ end of the string
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 3500
+
+app.use('/', express.static(path.join(__dirname, '/public')))
+
+app.use('/', require('./routes/root'))
+
+
+//throw
+app.all('*', (req, res) => {
+    res.status(400)
+    if (req.accepts('html')) {
+        res.sendfile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({message: "404 Not Found"})
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+// $ npm run dev
