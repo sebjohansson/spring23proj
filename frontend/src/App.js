3e33bd536ead7edef930122b27be31c3ca092027
Linna Jansson <linnajansson@Linnas-MacBook-Air.local>
2023-01-17 14:47:03 +0100

    init commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <p className="text-lg color-red-500 ">Welcome</p>
+      <div className="black text-2xl"> To the app component</div>
     </div>
   );
 }
