9d508c1dc3d66b24d22d5c6a7e86ee3dd77c4655
sebjohansson <sebastian.erik.johansson@gmail.com>
2023-01-24 11:07:56 +0100

    Added api functionality.

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 
 const cors = require('cors');
 const express = require('express');
+const dotenv = require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
+app.get('/api/quiz', (req, res) => {
+    res.status(200).json({message: 'Fetched'})
+})
 app.use(cors());
 app.use(express.json());
 
