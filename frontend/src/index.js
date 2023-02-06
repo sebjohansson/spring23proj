9722dc9e29d8fbe14575a1678ae0a7ef189b3ea4
Benny <gatts@live.se>
2023-02-06 10:34:12 +0100

    Fixed somethingr

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+      <div className="bg-hero-bg bg-cover">
+          <div className="min-h-screen min-w-screen">
+              <App />
+          </div>
+      </div>
     <App />
     <div className="text-2xl text-blue-400" >Index</div>
   </React.StrictMode>
