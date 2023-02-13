98eaaf80d3ffca1bfc8b40b5f114abf8da7d37c9
sebjohansson <sebastian.erik.johansson@gmail.com>
2023-02-13 10:54:14 +0100

    Added code for reward

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -2,8 +2,27 @@ import gold from "../assets/spaceslammedal-03.png"
 import bronze from "../assets/spaceslammedal-04.png"
 import silver from "../assets/spaceslammedal-05.png"
 import icon4 from "../assets/icon-12.svg";
+import {useState, useEffect} from "react";
 
 const Results = () => {
+    const maxScore=10
+    const score=7
+    const [status, setStatus] = useState(" ")
+    const ScoreHandler = () =>{
+        if (score >= 9){
+        setStatus(gold)
+    } else if (score <= 8 && score >= 6) {
+        setStatus(silver)
+    } else {
+        setStatus(bronze)
+    }
+        useEffect(()=>{
+            ScoreHandler()
+        }, [])
+    }
+
+
+
     return(
         <div>
             <section className="bg-purple-500 bg-opacity-50 outline-8 outline-dotted outline-purple-400 p-10 m-auto w-4/5 md:w-3/5 lg:w-2/5 rounded-lg pt-10 pb-10 text-center">
@@ -16,25 +35,12 @@ const Results = () => {
                 </h1>
                 <div className="grid grid-cols-1">
                     <p className="pb-5 pt-5 opacity-100 text-white font-semibold tracking-widest">
-                        You scored 5/5{" "}
+                        You scored {score}/{maxScore}
                     </p>
                 </div>
             </section>
         </div>
     )
-}
-function playerReward({ src, status }) {
-  return (
-    <div>
-      {
-        {
-          gold: <Gold src={gold} />,
-          silver: <Silver src={silver} />,
-          bronze: <Bronze src={bronze} />,
-        }[status]
-      }
-    </div>
-  );
 }
 
 export default Results
